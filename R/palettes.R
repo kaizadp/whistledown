e2effbef30e2521f8616629a0ea9cbd24e4f12d0
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2026-02-05 20:23:26 -0800

    intiial files for package

---
+++ R/palettes.R
@@ -0,0 +1,114 @@
+# Soils Color Palette Package
+# These are color palettes for RStudio
+# inspired by soil profiles.
+
+# 1.Create the color palettes
+# ::::::::::::::::::::::::::::::::::::::::::::::
+#' Complete list of palettes
+#'
+#' Use \code{names(soil_palettes)} to view list of palette names.
+#' Currently:  "alaquod" "durorthod"  "eutrostox" "natrudoll" "paleustalf" "podzol" "rendoll" "vitrizerand" "bangor" "pywell" "crait" "redox" "redox2" "gley"        
+#'
+#' @export
+whistledown_palettes <- list(
+  sharmaji = rbind(c('#DFD3DD', '#D59AAE', '#9A3268', '#652D76', '#8EB2C1'),c(5,4,1,2,3)),
+  red = rbind(c('#561e21', '#b3343d', '#B58B9D', '#D3C2B1', '#E4E4E4'),c(5,1,3,4,2)),
+  danbury = rbind(c('#29001B', '#952926', '#E4697E', '#C7A5DF', '#CADBE2'),c(5,3,1,2,4)),
+  featherington = rbind(c('#D8E0E9', '#7F989F', '#688844', '#E6AB47', '#D66F90'),c(5,4,3,1,2))
+)
+
+
+
+# 2. Palette builder function
+# ::::::::::::::::::::::::::::::::::::::::::::::
+
+#' Soil Palette Generator.
+#'
+#' This function builds palettes based on soil profiles.
+#' View photos for each palette \href{https://github.com/kaizadp/soilpalettes}{On Github}.
+#'
+#' @param name Name of the color palette. Options are \code{durorthod}, \code{rendoll}, \code{paleustalf},
+#' \code{natrudoll}, \code{alaquod2}, \code{alaquod}
+#' @param n Number of colors in the palette. Palletes include 5 colors, which can be used discretely,
+#' or if more are desired, used as a gradient. If omitted, n = length of palette.
+#' @param type Usage of palette as "continuous" or "discrete". Continuous usage interpolates between colors to create
+#' a scale of values. If omitted, function assumes continuous if n > length of palette, and discrete if n < length of palette.
+#' @param direction (TODO)
+#'
+#' @return TODO
+#' @export
+#'
+#' @examples
+#' whistledown_palette("featherington", n = 100, type = "continuous")
+#' whistledown_palette("featherington", 3)
+#' whistledown_palette("featherington", 50)
+whistledown_palette <- function(name, n, type = c("discrete", "continuous"), direction = 1) {
+  stopifnot(is.character(name))
+  stopifnot(is.numeric(n))
+  stopifnot(is.character(name))
+
+  pal <- whistledown_palettes[[name]]
+
+
+  if (is.null(pal)) {
+    stop("Palette not found.")
+  }
+
+  if (missing(n)) {
+    n <- length(pal[1, ])
+  }
+
+  if (missing(type)) {
+    if (n > length(pal[1, ])) {
+      type <- "continuous"
+    }
+    else {
+      type <- "discrete"
+    }
+  }
+  type <- match.arg(type)
+
+  if (type == "discrete" && n > length(pal[1, ])) {
+    stop("Number of requested colors greater than what discrete palette can offer, \n  use as continuous instead.")
+  }
+
+  if (abs(direction) != 1) {
+    stop("direction must be 1 or -1")
+  }
+
+  if (direction == -1) {
+    pal[1, ] <- rev(pal[1, ])
+  }
+
+  out <- switch(type,
+    continuous = grDevices::colorRampPalette(pal[1, ])(n),
+    discrete = pal[1, ][pal[2, ] %in% c(1:n)],
+  )
+  structure(out, class = "palette", name = name)
+}
+
+
+#' Palette Print Function
+#'
+#' @param x TODO
+#' @param ... TODO
+#' @importFrom graphics image par text rect
+#' @importFrom stats median
+#' @importFrom grDevices rgb
+#' @return TODO
+#' @export
+print.palette <- function(x, ...) {
+  stopifnot(class(x) == "palette")
+
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n),
+    col = x,
+    ylab = "", xaxt = "n", yaxt = "n", bty = "n"
+  )
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text(median(1:n), 1, labels = attr(x, "name"), cex = 1, family = "sans")
+}
